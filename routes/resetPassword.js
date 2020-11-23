58d517f16fe5371079cb7db75ca7aeda5c067177
Ethan Izenwasser <e@ethanizen.com>
2020-11-23 16:36:59 -0500

    Change mongo URI to environment variable for resetPassword.js

--- routes/resetPassword.js
+++ routes/resetPassword.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-const url = 'mongodb+srv://greymatterDB:BGRjw7aR8kfAQq0T@greymatter.we1hx.mongodb.net/GreyMatter?retryWrites=true&w=majority';
+const url = process.env.MONGO_URI;
 const mongoose = require('mongoose');
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true});
 
